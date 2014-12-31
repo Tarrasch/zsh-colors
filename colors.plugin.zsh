44614a88a4f573a16e1f0d5fd27f81f312176925
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2014-12-31 17:38:27 -0200

    autoloading colors by default

--- colors.plugin.zsh
+++ colors.plugin.zsh
@@ -3,6 +3,6 @@
 # https://wiki.archlinux.org/index.php/Zsh
 #
 # You can also debug this library with `whence -f red`
-
 colors=( black red green yellow blue magenta cyan white )
 autoload -Uz $colors
+autoload -U colors && colors
